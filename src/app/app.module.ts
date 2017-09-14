c3f7de18a46f1e5af33d1b312c86b51558638d36
Arkadiusz <asmilewski@gmail.com>
2017-09-14 17:35:19 +0200

    routing between the home page and listing page works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-
+import { Routes } from "@angular/router";
+import { RouterModule,} from '@angular/router';
 import { MyListComponent } from './my-list/my-list.component';
+import { Component } from '@angular/core';
+import { MyHomeComponent } from './my-home/my-home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'home',  component: MyHomeComponent },
+  { path: 'my-list', component: MyListComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyListComponent
+    MyListComponent,
+    MyHomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/my-home/my-home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-home',
+  template: `
+    <h1>
+      Check the list of our Cat Postcard and choose your favorite one!
+    </h1>
+  `,
+  styles: []
+})
+export class MyHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
